b9ea8f2a11cc1ca576208b19b31e1516378249cc
NestorDP <nestor-dp@hotmail.com>
2024-01-09 17:21:53 -0300

    Import URDF from littlebot_description package

--- launch/littlebot_gazebo_ignition.launch.py
+++ launch/littlebot_gazebo_ignition.launch.py
@@ -15,6 +15,9 @@ def generate_launch_description():
 
     littlebot_gazebo_path = os.path.join(
         get_package_share_directory('littlebot_gazebo'))
+    
+    littlebot_description_path = os.path.join(
+        get_package_share_directory('littlebot_description'))
 
     ros_gz_sim_path = os.path.join(
         get_package_share_directory('ros_gz_sim'))
@@ -22,9 +25,9 @@ def generate_launch_description():
     # World file name
     world_file = os.path.join(littlebot_gazebo_path, 'worlds', 'floor.sdf')
 
-    xacro_file = os.path.join(littlebot_gazebo_path,
+    xacro_file = os.path.join(littlebot_description_path,
                               'urdf',
-                              'littlebot.urdf.xacro')
+                              'littlebot_description.urdf.xacro')
 
     doc = xacro.parse(open(xacro_file))
     xacro.process_doc(doc)
